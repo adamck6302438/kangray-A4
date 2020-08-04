e9c1858b750650d52cf6480e5ecc607f5cf43efd
Ray Kang <adamck6302438@gmail.com>
2020-08-04 14:51:14 -0400

    Deployment for Assignment 4

---
+++ assets/data/myLogin.ts
@@ -0,0 +1,5 @@
+import { kangray } from '../../app/kangray';
+
+export const myLogin: kangray[] = [
+    {sid: 991530655, sname: 'Ray Kang', login:'kangray', campus: 'Trafalgar', assignmentTitle: "Assignment 4"}
+];
